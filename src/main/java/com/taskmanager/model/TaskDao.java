65307686e7660011accc5c15f0891e66111210cc
Alan <alanronimathew@gmail.com>
2026-05-12 21:14:52 +0400

    Done removing tasks done

--- src/main/java/com/taskmanager/model/TaskDao.java
+++ src/main/java/com/taskmanager/model/TaskDao.java
@@ -8,37 +8,46 @@ import org.hibernate.Transaction;
 
 import com.taskmanager.util.HibernateUtil;
 
-
 public class TaskDao {
 
-    
-
-    List<Task> tasks = new ArrayList<>();
-    
-    public String addTasks(Task t){
-        try{
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        session.persist(t);
-        transaction.commit();
-        session.close();
-        }
-        catch(Exception e){
+    public String addTasks(Task t) {
+        try {
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            Transaction transaction = session.beginTransaction();
+            session.persist(t);
+            transaction.commit();
+            session.close();
+        } catch (Exception e) {
             return e.getMessage();
         }
         return "Task added";
     }
 
+    public List<Task> viewTasks() {
 
-    public String removeTask(int i){
-        tasks.remove(i);
-        return "Task removed";
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        List<Task> tasks = session.createQuery("FROM Tasks", Task.class).list();
+        session.close();
+        return tasks;
     }
-    public String markTask(int i){
+
+    @SuppressWarnings("deprecation")
+    public String removeTask(int i) {
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.createMutationQuery("Delete from Tasks where id = :id").setParameter("id", i).executeUpdate();
+            transaction.commit();
+        }
+        return "Task Removed";
+    }
+
+    public String markTask(int i) {
         tasks.get(i).setStatus(true);
         return "Task marked as done";
     }
-    public String updateTask(int i, String desc){
+
+    public String updateTask(int i, String desc) {
         tasks.get(i).setTask(desc);
         return "Task " + i + " updated";
     }
